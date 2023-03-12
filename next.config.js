f9ab657de2e945a0794e552e4b51118f9c1595cc
Alison Martinez <alison.a.martinez@outlook.com>
2023-03-11 22:14:47 -0500

    following config steps

--- next.config.js
+++ next.config.js
@@ -1,14 +1,27 @@
 const withMarkdoc = require('@markdoc/next.js')
 
 /** @type {import('next').NextConfig} */
+
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+let repo
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+}
+
 const nextConfig = {
+  assetPrefix: `/${repo}/`,
+  basePath: `/${repo}`,
   reactStrictMode: true,
   pageExtensions: ['js', 'jsx', 'md'],
+  images: {
+    unoptimized: true,
+  },
   experimental: {
     scrollRestoration: true,
-    images: {
-      unoptimized: true,
-    },
   },
 }
 
